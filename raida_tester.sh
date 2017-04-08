4446b1728fe3b87f85a03b2b0ce3b849b79be41d
A-Lang <alang.hsu@gmail.com>
2017-04-08 20:40:10 +0800

    Added the option advanced and fixed some minor bugs

--- raida_tester.sh
+++ raida_tester.sh
@@ -11,7 +11,7 @@ _GREEN_='\033[32m'
 _RED_='\033[31m'
 _BOLD_='\033[1m'
 CURL_CMD="curl"
-CURL_OPT="-qSfs"
+CURL_OPT="-qSfs -m 60"
 JQ_CMD="jq"
 
 
@@ -23,7 +23,7 @@ RUNNINGARCH=$(`echo uname -m` | tr '[a-z]' '[A-Z]')
 
 # Strings
 string_01="Could not Check hints because get_ticket service failed to get a ticket. Fix get_ticket first."
-string_02="Loading test coin : $WORKDIR/$testcoin"
+string_02="Loading test coin: $WORKDIR/$testcoin"
 string_03="Checking ticket..."
 string_04="Empty ticket"
 string_05="HTTPS Access No Response"
@@ -71,6 +71,7 @@ RAIDA Tester Commands Available:
 [+] get_ticket (g)
 [+] hints      (h)
 [+] fix        (f)
+[+] advanced   (a)
 [+] quit       (q)
 EOF
 
@@ -119,6 +120,9 @@ Main()
         elif [ "$input" == "fix" -o "$input" == "f" ];then
             Process_request _fix
 
+        elif [ "$input" == "advanced" -o "$input" == "a" ];then
+            Advanced
+
         elif [ "$input" == "quit" -o "$input" == "q" ];then
             break
 
@@ -166,6 +170,42 @@ Get_denom(){
     echo $denom
 }
 
+Advanced(){
+    input=""
+    PROMPT="ADVANCED"
+    while true
+    do
+        echo "Test All RAIDA Nodes [0-5]: 1.Echo 2.Detect 3.Ticket 4.Hints 5.Fix 0.Exit"
+        echo "NOTE: This process may take a few mins to check all nodes please be patient until all check done."
+        echo -n "$PROMPT> " && read input
+        if [ $input -ge 1 -a $input -le 5 ] 2>/dev/null ;then
+            case "$input" in
+                1)
+                     _all_echo
+                     ;;
+                2)
+                    _all_detect
+                     ;;
+                3)
+                    _all_ticket
+                    ;;
+                4)
+                    _all_hints
+                    ;;
+                5)
+                    _all_fix
+                    ;;
+            esac
+
+        elif [ $input -eq 0 ] 2>/dev/null ;then
+            break
+
+        else
+            Error "$error_02"
+
+        fi
+    done
+}
 
 Process_request(){
     input=""
@@ -196,10 +236,10 @@ Process_request(){
     do
         echo "What RAIDA# do you want to test $PROMPT? Enter 25 to end."
         echo -n "$PROMPT> " && read input
-        if [ $input -ge 0 -a $input -lt 25  ];then
+        if [ $input -ge 0 -a $input -lt 25  ] 2>/dev/null;then
             $option $input
 
-        elif [ "$input" = "$raida_nums" ];then
+        elif [ "$input" -eq "$raida_nums" ] 2>/dev/null;then
             break
 
         else
@@ -207,6 +247,22 @@ Process_request(){
 
         fi
     done
+}
+
+_all_echo(){
+    echo -n "ECHO Results: "
+    for ((n=0;n<$raida_nums;n++))
+    do
+        _echo $n >/dev/null 2>&1
+        run_echo=$?
+        if [ $run_echo -eq 0 ];then
+            status="pass"
+        else
+            status="${_RED_}fail${_REST_}"
+        fi 
+        echo -e -n "-> RAIDA#${n}: ${status}(${elapsed}ms)  "
+    done
+    echo;echo
 }
 
 _echo()
@@ -246,6 +302,26 @@ _echo()
     return $echo_retval
 }
 
+_all_detect(){
+    # Check the local testcoin file
+    Load_testcoin
+    is_testcoin=$?
+    [ $is_testcoin -eq 1 ] && return 1  # testcoin file not found or with wrong format
+
+    echo -n "DETECT Results: "
+    for ((n=0;n<$raida_nums;n++))
+    do
+        _detect $n >/dev/null 2>&1
+        run_detect=$?
+        if [ $run_detect -eq 0 ];then
+            status="pass"
+        else
+            status="${_RED_}fail${_REST_}"
+        fi 
+        echo -e -n "-> RAIDA#${n}: ${status}(${elapsed}ms)  "
+    done
+    echo;echo
+}
 
 _detect(){
     # Check the local testcoin file
@@ -305,6 +381,27 @@ _detect(){
 
 }
 
+_all_ticket(){
+    # Check the local testcoin file
+    Load_testcoin
+    is_testcoin=$?
+    [ $is_testcoin -eq 1 ] && return 1  # testcoin file not found or with wrong format
+
+    echo -n "TICKET Results: "
+    for ((n=0;n<$raida_nums;n++))
+    do
+        _get_ticket $n >/dev/null 2>&1
+        run_ticket=$?
+        if [ $run_ticket -eq 0 ];then
+            status="pass"
+        else
+            status="${_RED_}fail${_REST_}"
+        fi 
+        echo -e -n "-> RAIDA#${n}: ${status}(${elapsed}ms)  "
+    done
+    echo;echo
+
+}
 
 _get_ticket(){
     # Check the local testcoin file
@@ -372,6 +469,27 @@ _get_ticket(){
 
 }
 
+_all_hints(){
+    # Check the local testcoin file
+    Load_testcoin
+    is_testcoin=$?
+    [ $is_testcoin -eq 1 ] && return 1  # testcoin file not found or with wrong format
+
+    echo -n "HINTS Results: "
+    for ((n=0;n<$raida_nums;n++))
+    do
+        _hints $n > /dev/null 2>&1
+        run_hints=$?
+        if [ $run_hints -eq 0 ];then
+            status="pass"
+        else
+            status="${_RED_}fail${_REST_}"
+        fi 
+        echo -e -n "-> RAIDA#${n}: ${status}(${elapsed}ms)  "
+    done
+    echo;echo
+
+}
 
 _hints(){
     Load_testcoin
@@ -455,6 +573,28 @@ _hints(){
     return $hints_retval
 }
 
+_all_fix(){
+    local i
+    local j
+    local n
+    
+    # Check the local testcoin file
+    Load_testcoin
+    is_testcoin=$?
+    [ $is_testcoin -eq 1 ] && return 1  # testcoin file not found or with wrong format
+
+    echo
+    echo "FIX Results [Fix1,Fix2,Fix3,Fix4]: "
+    for ((n=0;n<$raida_nums;n++))
+    do
+        echo -n "-> RAIDA#${n}: "
+        _fix_all_corners $n  
+
+    done
+    echo;echo
+
+}
+
 _fix(){
     Load_testcoin
     is_testcoin=$?
@@ -467,15 +607,6 @@ _fix(){
     array_an=( $string_an )
     denom=$(Get_denom $sn)
      
-    #declare -a array_fix_corner1
-    #declare -a array_fix_corner2
-    #declare -a array_fix_corner3
-    #declare -a array_fix_corner4
-    #declare -a array_trusted_servers
-    #declare -a fromserver
-    #declare -a message
-    #declare -a get_ticket_status
-
     array_fix_corner1[1]=$(( fixed_server - 1))
     array_fix_corner1[2]=$(( fixed_server - 5))
     array_fix_corner1[3]=$(( fixed_server - 6))
@@ -549,15 +680,16 @@ _fix(){
                 elapsed=$(( (end_s-start_s)/1000000 ))
 
                 if [ $http_retval -eq 0 ]; then
-                    status=$(echo $http_response | $JQ_CMD -r '.status')
+                    status=$(echo $http_response | sed 's/\\/\//g' | sed 's/.* \({.*}\)$/\1/' | $JQ_CMD -r '.status')
                 else
                     status="error"
                     fix_retval=1
                 fi
 
-                if [ $status == "success" ];then
+                if [ "$status" == "success" ];then
                     echo
                     echo -e "Status: $_BOLD_$_GREEN_$status$_REST_"
+                    echo "Milliseconds: $elapsed"
                     echo "Request: $raida_url"
                     echo -e "Response: $_GREEN_$http_response$_REST_"
                     echo
@@ -565,6 +697,7 @@ _fix(){
                 else
                     echo
                     echo -e "Status: $_BOLD_$_RED_$status$_REST_"
+                    echo "Milliseconds: $elapsed"
                     echo "Request: $raida_url"
                     echo -e "Response: $_RED_$http_response$_REST_"
                     echo
@@ -582,6 +715,115 @@ _fix(){
         else
             Error "$error_02"
 
+        fi
+    done
+
+}
+
+_fix_all_corners(){
+    local c
+    local i
+    local j
+    local n
+
+    fixed_server=$1
+    nn=`$JQ_CMD '.cloudcoin[].nn' $testcoin | tr -d '"'`
+    sn=`$JQ_CMD '.cloudcoin[].sn' $testcoin | tr -d '"'`
+    string_an=`$JQ_CMD -r '.cloudcoin[].an[]' $testcoin`
+    array_an=( $string_an )
+    denom=$(Get_denom $sn)
+     
+    array_fix_corner1[1]=$(( fixed_server - 1))
+    array_fix_corner1[2]=$(( fixed_server - 5))
+    array_fix_corner1[3]=$(( fixed_server - 6))
+    array_fix_corner2[1]=$(( fixed_server + 1))
+    array_fix_corner2[2]=$(( fixed_server - 4))
+    array_fix_corner2[3]=$(( fixed_server - 5))
+    array_fix_corner3[1]=$(( fixed_server - 1))
+    array_fix_corner3[2]=$(( fixed_server + 4))
+    array_fix_corner3[3]=$(( fixed_server + 5))
+    array_fix_corner4[1]=$(( fixed_server + 1))
+    array_fix_corner4[2]=$(( fixed_server + 5))
+    array_fix_corner4[3]=$(( fixed_server + 6))
+
+    for ((i=1;i<=4;i++))
+    do
+        array_name="array_fix_corner$i"
+        n=1
+        for j in $(eval echo \${$array_name[@]})
+        do
+            if [ $j -lt 0 ];then
+                eval $array_name[$n]=$(( $j + 25))
+            elif [ $j -gt 24 ];then
+                eval $array_name[$n]=$(( $j - 25))
+            fi
+            ((n++))
+        done
+    done
+
+    
+    for ((c=1;c<=4;c++))
+    do
+        array_name="array_fix_corner$c"
+        array_trusted_servers=$(eval echo \${$array_name[@]})
+        n=1
+        Fix_ticket_retval=0
+
+        for i in ${array_trusted_servers[@]}
+        do
+            raida="raida$i"
+            an="${array_an[$i]}"
+            raida_url="https://$raida.cloudcoin.global/service/get_ticket"
+            raida_url="$raida_url?nn=$nn&sn=$sn&toserver=$fixed_server&an=$an&pan=$an&denomination=$denom"
+                
+            Fix_ticket_request $raida_url >/dev/null 2>&1
+            retval=$?
+
+            if [ $retval -eq 0 ];then
+                fromserver[$n]="$i"
+                message[$n]="$ticket"
+                get_ticket_status[$n]="$status"
+            else
+                get_ticket_status[$n]="empty"
+                Fix_ticket_retval=1   
+            fi
+
+            ((n++))
+        done
+        
+        if [ $Fix_ticket_retval -eq 0 ]; then
+            sleep 1
+            raida="raida$fixed_server"
+            an="${array_an[$fixed_server]}"
+            raida_url="https://$raida.cloudcoin.global/service/fix"
+            raida_url="$raida_url?fromserver1=${fromserver[1]}&message1=${message[1]}&fromserver2=${fromserver[2]}&message2=${message[2]}&fromserver3=${fromserver[3]}&message3=${message[3]}&pan=$an"
+            start_s=$(Timer)
+            http_response=$($CURL_CMD $CURL_OPT $raida_url 2>&1)
+            http_retval=$?
+            end_s=$(Timer)
+            elapsed=$(( (end_s-start_s)/1000000 ))
+
+            if [ $http_retval -eq 0 ]; then
+                status=$(echo $http_response | sed 's/\\/\//g' | sed 's/.* \({.*}\)$/\1/' | $JQ_CMD -r '.status')
+                fix_retval=0
+            else
+                status="error"
+                fix_retval=1
+            fi
+
+            if [ "$status" != "success" ];then
+                fix_retval=1
+            fi
+
+        else
+            fix_retval=1
+
+        fi
+        
+        if [ $fix_retval -eq 0 ];then
+            [ $c -eq 4 ] && echo "pass" || echo -n "pass, "
+        else
+            [ $c -eq 4 ] && echo -e "${_RED_}fail${_REST_}" || echo -e -n "${_RED_}fail${_REST_}, "
         fi
     done
 
